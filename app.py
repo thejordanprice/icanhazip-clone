1e6b84951b0818e62d888cfa52cb9bdd6731f6c7
thejordanprice <thejordanprice@gmx.com>
2024-07-22 00:25:39 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_ip():
-    return request.remote_addr
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+    return ip
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
