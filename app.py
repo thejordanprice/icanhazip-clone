d860df00fc3f713b5e39dec1a75d92bce59ee2ac
thejordanprice <thejordanprice@gmx.com>
2024-07-23 02:20:33 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def get_ip():
     # Get the CF-Connecting-IP header if present
     cf_ip = request.headers.get("CF-Connecting-IP")
     if cf_ip:
-        # Check if the CF-Connecting-IP is a valid IPv6 address
         try:
             ip = ipaddress.ip_address(cf_ip)
             if ip.version == 4:
@@ -16,15 +15,18 @@ def get_ip():
         except ValueError:
             pass  # Not a valid IP address
 
-    # Fallback to X-Forwarded-For header
-    x_forwarded_for = request.headers.getlist("X-Forwarded-For")
-    for ip in x_forwarded_for:
-        try:
-            ip_obj = ipaddress.ip_address(ip)
-            if ip_obj.version == 4:
-                return ip  # Return if it's an IPv4 address
-        except ValueError:
-            pass  # Not a valid IP address
+    # Check the X-Forwarded-For header
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        # The X-Forwarded-For header can contain multiple IPs, we need the first one
+        for ip in x_forwarded_for.split(','):
+            ip = ip.strip()
+            try:
+                ip_obj = ipaddress.ip_address(ip)
+                if ip_obj.version == 4:
+                    return ip  # Return if it's an IPv4 address
+            except ValueError:
+                pass  # Not a valid IP address
 
     # Fallback to request.remote_addr
     return request.remote_addr
